9093b3ed86bb703895d454c8bff4e0e69fe1c676
agdgom18 <agdgomelashvili18@gmail.com>
2023-08-29 21:11:24 +0400

    added components

---
+++ src/components/Categories.tsx
@@ -0,0 +1,18 @@
+const Categories = () => {
+  return (
+    <>
+      <div className="categories">
+        <ul>
+          <li className="active">Все</li>
+          <li>Meat</li>
+          <li>Vegetarian</li>
+          <li>Grill</li>
+          <li>Spicy</li>
+          <li>Closed</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Categories;
