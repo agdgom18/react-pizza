d415f6c5195ae9f8d219d0d0fb7ca2e4b7840abd
agdgom18 <agdgomelashvili18@gmail.com>
2023-08-30 12:30:14 +0400

    solve console errors and add db.json

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,14 +1,24 @@
+import React from 'react';
+
 const Categories = () => {
+  const categoryList = ['All', 'Meat', 'Vegetarian', 'Grill', 'Spicy', 'Closed'];
+  const [activeCategory, setActiveCategory] = React.useState(0);
+
+  const onClickCategory = (index: number) => {
+    setActiveCategory(index);
+  };
+
   return (
     <>
       <div className="categories">
         <ul>
-          <li className="active">Все</li>
-          <li>Meat</li>
-          <li>Vegetarian</li>
-          <li>Grill</li>
-          <li>Spicy</li>
-          <li>Closed</li>
+          {categoryList.map((el, index) => {
+            return (
+              <li onClick={() => onClickCategory(index)} className={activeCategory === index ? 'active' : ''} key={index}>
+                {el}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
--- src/components/PizzaBlock.tsx
+++ src/components/PizzaBlock.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
+
 const PizzaBlock = () => {
+  const [count, setCount] = React.useState(0);
   return (
     <>
       <div className="pizza-block">
@@ -21,7 +24,7 @@ const PizzaBlock = () => {
         </div>
         <div className="pizza-block__bottom">
           <div className="pizza-block__price">from 20 $</div>
-          <div className="button button--outline button--add">
+          <button onClick={() => setCount(count + 1)} className="button button--outline button--add">
             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
@@ -29,8 +32,8 @@ const PizzaBlock = () => {
               />
             </svg>
             <span>Add</span>
-            <i>2</i>
-          </div>
+            <i>{count}</i>
+          </button>
         </div>
       </div>
     </>
