56741f50fd8240f665988e1f54ad2cedbdcd07f2
agdgom18 <agdgomelashvili18@gmail.com>
2023-09-01 20:03:06 +0400

    Connected the frontend to the backend with mockAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import React from 'react';
+
 import './scss/app.scss';
 import Header from './components/Header.jsx';
 import Categories from './components/Categories.js';
 import Sort from './components/Sort.js';
 import PizzaBlock from './components/PizzaBlock.js';
-import pizzasItems from './utils/db.json';
-
-console.log(pizzasItems);
 
 function App() {
+  // https://64f1da430e1e60602d245dfa.mockapi.io/items
+
+  interface Pizza {
+    id: number;
+    imageUrl: string;
+    name: string;
+    types: number[];
+    sizes: number[];
+    price: number;
+    category: number;
+    rating?: number;
+  }
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://64f1da430e1e60602d245dfa.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr: Pizza[]) => setItems(arr));
+  }, []);
   return (
     <>
       <div className="wrapper">
@@ -15,12 +33,12 @@ function App() {
         <div className="content">
           <div className="container">
             <div className="content__top">
-              {/* <Categories /> */}
+              <Categories />
               <Sort />
             </div>
             <h2 className="content__title">All pizzes</h2>
             <div className="content__items">
-              {pizzasItems.map((el) => {
+              {items.map((el) => {
                 return <PizzaBlock key={el.id} {...el}></PizzaBlock>;
               })}
             </div>
